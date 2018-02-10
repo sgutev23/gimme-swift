f6947f7f3b928d91266a939e3cfd8efe4d78976c
Stan Gutev <stan@stan.fritz.box>
2018-02-10 20:39:21 +0100

    first

---
+++ gimme/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  gimme
+//
+//  Created by Stan Gutev on 10.02.18.
+//  Copyright © 2018 lepookey. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
